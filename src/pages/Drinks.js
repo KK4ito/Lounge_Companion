f85feac5490e53cb618b0697600b6d0959c0c96b
Daellen <david.aellen@students.fhnw.ch>
2016-12-09 15:06:12 +0100

    changed Drink Game and Home

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,7 +2,7 @@
  * Created by David on 09/12/2016.
  */
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, ListGroup, ListGroupItem, Accordion, Panel, Table} from 'react-bootstrap';
 
 
 export default class Drinks extends React.Component {
@@ -10,14 +10,27 @@ export default class Drinks extends React.Component {
         super(props);
         this.state = {
             Title: 'Drinks',
-            Drinks: []
+            Drinks: [{ name: 'Feldschloesschen',size: 5, price: 0.0, category: 1}]
         };
     }
     render() {
         return (
             <Jumbotron>
                 <h1>{this.state.Title}</h1>
-                <li></li>
+                <Accordion>
+                    <Panel header="Bier" eventKey="1">
+                        <Table responsive>
+                            <tr>
+
+                            </tr>
+                        </Table>
+                    </Panel>
+                    <Panel header="Softdrinks" eventKey="2">
+                        <ListGroup>
+                            <ListGroupItem></ListGroupItem>
+                        </ListGroup>
+                    </Panel>
+                </Accordion>
             </Jumbotron>
         );
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
  * Created by David on 09/12/2016.
  */
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 
 export default class Home extends React.Component {
@@ -12,19 +12,27 @@ export default class Home extends React.Component {
             Title: 'Novum Lounge',
             SubtitleOpen: 'Oeffnungszeiten',
             SubtitleEvents: 'Events',
-            Description: 'Tabs',
-            OpenTime: 0,
+            Description: 'Wir sind die Studentenbar am Campus Brugg-Windisch der FHNW. Die wahren Studenten unter euch finden sich in der Vorlesungszeit jeden Donnerstag und Freitag ab 16:00 Uhr hinter dem Gebäude 4 wieder.',
+            OpenTime: ['Do: 4pm - 2am','Fr: 4pm - 2am'],
+            Events: ['Toeggele Turnier: 18.11.16']
         };
     }
+
+
+
     render() {
         return (
             <Jumbotron>
                 <h1>{this.state.Title}</h1>
                 <p>{this.state.Description}</p>
                 <h2>{this.state.SubtitleOpen}</h2>
-                <p>{this.state.OpenTime}</p>
+                <ListGroup>{this.state.OpenTime.map(function(time, index){
+                    return <ListGroupItem key={index}>{time}</ListGroupItem>;
+                })}</ListGroup>
                 <h2>{this.state.SubtitleEvents}</h2>
-                <li></li>
+                <ListGroup>{this.state.Events.map(function(event, index){
+                    return <ListGroupItem key={index}>{event}</ListGroupItem>;
+                })}</ListGroup>
             </Jumbotron>
         );
     }
