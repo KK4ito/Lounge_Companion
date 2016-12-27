d35af921eb661a66e587904beb3bad9b58e9b451
Kevin <k4ito@outlook.com>
2016-12-27 15:59:51 +0100

    fixed drinkspage

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -10,8 +10,6 @@ export default class Drinks extends React.Component {
     super(props);
     this.state = {
       Title: 'Drinks',
-      //    Drinks: [{ name: 'Feldschloesschen',size: 5, price: 4.0, category: 1, id: 1},
-      //        {name: 'Cola', size: 2.5, price: 3.0, category: 2, id: 2}],
       Drinks: [],
       DrinksServer: [],
       DrinksCategory: [],
@@ -22,46 +20,36 @@ export default class Drinks extends React.Component {
     fetch(this.state.url + '/drinkcategories', {
       method: 'GET'
     })
-    .then(result => {
-      console.log(result);
-      console.log(result.headers);
-      console.log(result.body);
-      console.log('categoresi');
-      this.state.DrinksCategory=result.json();
+    .then(response => response.json())
+    .then(json => {
+      this.setState({DrinksCategory: json});
     });
+
     fetch(this.state.url + '/drinks', {
       method: 'GET'
     })
-    .then(result=> {
-
-      console.log(result);
-      console.log('drinks');
-      this.state.DrinksServer=result.json();
-
+    .then(response=> response.json())
+    .then(json => {
+      this.setState({DrinksServer: json});
     });
   }
-  /*
-  <div>Items:</div>
-  { this.state.items.map(item=> { return <div>{http://item.name}</div>}) }
-  </div>
-  */
 
   render() {
     return (
       <Jumbotron>
         <h1>{this.state.Title}</h1>
         <Accordion>
-          {this.state.DrinksCategory.map(cat=>{
-            return <Panel header={cat.name} eventKey={cat.id}>
+          {this.state.DrinksCategory.map(cat => {
+            return <Panel header={cat.name} key={cat.id}>
               <Table responsive>
                 <tbody>
-                  {this.state.DrinksServer.map(drink=>{
-                    if(drink.category == cat.id){
+                  {this.state.DrinksServer.map(drink => {
+                    if(drink.categoryid == cat.id){
                       return (
                         <tr key={drink.id}>
                           <td>{drink.name}</td>
                           <td>{drink.size}</td>
-                          <td>{drink.price}</td>
+                          <td>{drink.price} .-</td>
                         </tr>
                       )
                     }
@@ -70,42 +58,6 @@ export default class Drinks extends React.Component {
               </Table>
             </Panel>
           })}
-        </Accordion>
-        <Accordion>
-          <Panel header="Bier" eventKey="1">
-            <Table responsive>
-              <tbody>
-                {this.state.Drinks.map(function (drink) {
-                  if(drink.category == 1){
-                    return (
-                      <tr key={drink.id}>
-                        <td >{drink.name}</td>
-                        <td >{drink.size}</td>
-                        <td >{drink.price}</td>
-                      </tr>
-                    );
-                  }
-                })}
-              </tbody>
-            </Table>
-          </Panel>
-          <Panel header="Softdrinks" eventKey="2">
-            <Table responsive>
-              <tbody>
-                {this.state.Drinks.map(function (drink) {
-                  if(drink.category == 2){
-                    return (
-                      <tr key={drink.id}>
-                        <td >{drink.name}</td>
-                        <td >{drink.size}</td>
-                        <td >{drink.price}</td>
-                      </tr>
-                    );
-                  }
-                })}
-              </tbody>
-            </Table>
-          </Panel>
         </Accordion>
       </Jumbotron>
     );
