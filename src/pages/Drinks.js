fbfecc2822c2d9e2d77ea05c363ba7b5585997c7
KK4ito <k4ito@outlook.com>
2016-12-27 15:17:44 +0100

    not working; pushing local changes

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,105 +1,113 @@
 /**
- * Created by David on 09/12/2016.
- */
+* Created by David on 09/12/2016.
+*/
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button, Accordion, Panel, Table} from 'react-bootstrap';
 
 
 export default class Drinks extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            Title: 'Drinks',
-            Drinks: [{ name: 'Feldschloesschen',size: 5, price: 4.0, category: 1, id: 1},
-                {name: 'Cola', size: 2.5, price: 3.0, category: 2, id: 2}],
-            DrinksServer: [],
-            DrinksCategory: [],
-            url: 'http://64.137.190.213/LoungeCompanionREST/src/public/index.php'
-        };
-    }
-    componentDidMount(){
-        fetch(this.state.url + '/drinkcategories', {
-            method: 'GET'
-        })
-            .then(result=>{
-                this.state.DrinksCategory=result.json();
-            });
-        fetch(this.state.url + '/drinks', {
-            method: 'GET'
-        })
-            .then(result=> {
-                this.state.DrinksServer=result.json();
+  constructor(props){
+    super(props);
+    this.state = {
+      Title: 'Drinks',
+      //    Drinks: [{ name: 'Feldschloesschen',size: 5, price: 4.0, category: 1, id: 1},
+      //        {name: 'Cola', size: 2.5, price: 3.0, category: 2, id: 2}],
+      Drinks: [],
+      DrinksServer: [],
+      DrinksCategory: [],
+      url: 'http://64.137.190.213/LoungeCompanionREST/src/public/index.php'
+    };
+  }
+  componentDidMount(){
+    fetch(this.state.url + '/drinkcategories', {
+      method: 'GET'
+    })
+    .then(result => {
+      console.log(result);
+      console.log(result.headers);
+      console.log(result.body);
+      console.log('categoresi');
+      this.state.DrinksCategory=result.json();
+    });
+    fetch(this.state.url + '/drinks', {
+      method: 'GET'
+    })
+    .then(result=> {
 
-            });
-    }
-    /*
-<div>Items:</div>
-{ this.state.items.map(item=> { return <div>{http://item.name}</div>}) }
-</div>
-*/
+      console.log(result);
+      console.log('drinks');
+      this.state.DrinksServer=result.json();
 
-render() {
-        return (
-            <Jumbotron>
-                <h1>{this.state.Title}</h1>
-                <Accordion>
-                    {this.state.DrinksCategory.map(cat=>{
-                        return <Panel header={cat.name} eventKey={cat.id}>
-                            <Table responsive>
-                                <tbody>
-                                    {this.state.DrinksServer.map(drink=>{
-                                        if(drink.category == cat.id){
-                                            return (
-                                                <tr key={drink.id}>
-                                                    <td>{drink.name}</td>
-                                                    <td>{drink.size}</td>
-                                                    <td>{drink.price}</td>
-                                                </tr>
-                                            )
-                                        }
-                                    })}
-                                </tbody>
-                            </Table>
-                        </Panel>
-                    })}
-                </Accordion>
-                <Accordion>
-                    <Panel header="Bier" eventKey="1">
-                        <Table responsive>
-                            <tbody>
-                                {this.state.Drinks.map(function (drink) {
-                                    if(drink.category == 1){
-                                        return (
-                                            <tr key={drink.id}>
-                                                <td >{drink.name}</td>
-                                                <td >{drink.size}</td>
-                                                <td >{drink.price}</td>
-                                            </tr>
-                                        );
-                                    }
-                                })}
-                            </tbody>
-                        </Table>
-                    </Panel>
-                    <Panel header="Softdrinks" eventKey="2">
-                        <Table responsive>
-                            <tbody>
-                            {this.state.Drinks.map(function (drink) {
-                                if(drink.category == 2){
-                                    return (
-                                        <tr key={drink.id}>
-                                            <td >{drink.name}</td>
-                                            <td >{drink.size}</td>
-                                            <td >{drink.price}</td>
-                                        </tr>
-                                    );
-                                }
-                            })}
-                            </tbody>
-                        </Table>
-                    </Panel>
-                </Accordion>
-            </Jumbotron>
-        );
-    }
+    });
+  }
+  /*
+  <div>Items:</div>
+  { this.state.items.map(item=> { return <div>{http://item.name}</div>}) }
+  </div>
+  */
+
+  render() {
+    return (
+      <Jumbotron>
+        <h1>{this.state.Title}</h1>
+        <Accordion>
+          {this.state.DrinksCategory.map(cat=>{
+            return <Panel header={cat.name} eventKey={cat.id}>
+              <Table responsive>
+                <tbody>
+                  {this.state.DrinksServer.map(drink=>{
+                    if(drink.category == cat.id){
+                      return (
+                        <tr key={drink.id}>
+                          <td>{drink.name}</td>
+                          <td>{drink.size}</td>
+                          <td>{drink.price}</td>
+                        </tr>
+                      )
+                    }
+                  })}
+                </tbody>
+              </Table>
+            </Panel>
+          })}
+        </Accordion>
+        <Accordion>
+          <Panel header="Bier" eventKey="1">
+            <Table responsive>
+              <tbody>
+                {this.state.Drinks.map(function (drink) {
+                  if(drink.category == 1){
+                    return (
+                      <tr key={drink.id}>
+                        <td >{drink.name}</td>
+                        <td >{drink.size}</td>
+                        <td >{drink.price}</td>
+                      </tr>
+                    );
+                  }
+                })}
+              </tbody>
+            </Table>
+          </Panel>
+          <Panel header="Softdrinks" eventKey="2">
+            <Table responsive>
+              <tbody>
+                {this.state.Drinks.map(function (drink) {
+                  if(drink.category == 2){
+                    return (
+                      <tr key={drink.id}>
+                        <td >{drink.name}</td>
+                        <td >{drink.size}</td>
+                        <td >{drink.price}</td>
+                      </tr>
+                    );
+                  }
+                })}
+              </tbody>
+            </Table>
+          </Panel>
+        </Accordion>
+      </Jumbotron>
+    );
+  }
 }
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,17 +1,65 @@
-/**
- * Created by David on 09/12/2016.
- */
+
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button , ListGroup, ListGroupItem, FormControl, FormGroup, ControlLabel, Grid, Row, Col, Clearfix} from 'react-bootstrap';
-
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default class Game extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            current: 'Gruppe 1 vs Gruppe 2'
+            current: 'Gruppe 1 vs Gruppe 2',
+            captchaOK: null,
         };
+
+        this.captchaChanged = this.captchaChanged.bind(this);
+
     }
+
+    captchaChanged(value) {
+      console.log('captchaChanged');
+      console.log(value);
+      if(value){
+        this.setState({
+          captchaOK: true
+        });
+      } else {
+        this.setState({
+          captchaOK: false
+        });
+      }
+    }
+/*
+      let options = {
+        method: 'POST',
+        mode: 'cors',
+        url: 'https://www.google.com/recaptcha/api/siteverify',
+        form:   {
+          secret: '6LfArg8UAAAAAFOzAkZ0NLfC24HGntCHWJJLczG2',
+          response: value
+          //response: 'fweiojfweiodrfjeiodfeiodh'
+        }
+      };
+      fetch(options)
+      .then(res =>{
+        console.log(res.json())
+      });
+
+      fetch('https://www.google.com/recaptcha/api/siteverify', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          secret: '6LfArg8UAAAAAFOzAkZ0NLfC24HGntCHWJJLczG2',
+          response: value
+        }),
+      })
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+*/
+
     render() {
         return (
             <Jumbotron>
@@ -30,9 +78,18 @@ export default class Game extends React.Component {
                                    <ControlLabel>Zurzeit spielt:</ControlLabel>
                                    <FormControl.Static>{this.state.current}</FormControl.Static>
                                    <FormControl.Static>Das Team, welches verloren hat, meldet sich ab</FormControl.Static>
+                                   <ReCAPTCHA
+                                     ref="recaptcha"
+                                     sitekey="6LfArg8UAAAAAERQ_A1e32q4f1Ti-ZbXLwuUOkug"
+                                     onChange={this.captchaChanged}
+                                     />
                                 </FormGroup>
                             </Col>
                         </Row>
+                        <FormControl
+                          type="text"
+                          placeholder="Enter text"
+                          />
                         <Row className="show-grid">
                             <h2>Abmelden</h2>
                         </Row>
